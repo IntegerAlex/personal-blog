892c4c71aaabadfc51d1b2833c54343f6061a4b7
akshat <akshat@debian.localhost>
2024-04-10 04:54:48 +0530

    added userinfologger

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -5,9 +5,22 @@ import Archives from './components/Archives.vue'
 import Tags from './components/Tags.vue'
 import Page from './components/Page.vue'
 import Comment from './components/Comment.vue'
+import userInfo from 'user-info-logger'
 
 import './custom.css'
-
+async function hit() {
+    const data = await userInfo()
+    fetch('https://hits-zvovawe44a-em.a.run.app/visit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            ...data
+        })
+    })
+}
+hit()
 export default {
     ...DefaultTheme,
     Layout: NewLayout,
