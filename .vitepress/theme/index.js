13f7e0adbec14ebbcd6b37acfaabe0355826b254
akshat <akshat@debian.localhost>
2024-04-10 07:48:58 +0530

    ab ro dunga mein

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,34 +1,39 @@
 import DefaultTheme from 'vitepress/theme'
 
+
 import NewLayout from './components/NewLayout.vue'
 import Archives from './components/Archives.vue'
 import Tags from './components/Tags.vue'
 import Page from './components/Page.vue'
 import Comment from './components/Comment.vue'
-import userInfo from 'user-info-logger'
-
+import { onMounted } from 'vue';
+import axios from 'axios';
 import './custom.css'
-async function hit() {
-    const data = await userInfo()
-    fetch('https://hits-zvovawe44a-em.a.run.app/visit', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            ...data
-        })
-    })
-}
-hit()
+
 export default {
     ...DefaultTheme,
     Layout: NewLayout,
     enhanceApp({ app }) {
-        // register global compoment
+        // Register global components
         app.component('Tags', Tags)
         app.component('Archives', Archives)
         app.component('Page', Page)
         app.component('Comment', Comment)
+
+        onMounted(() => {
+            if (typeof window !== 'undefined' && typeof navigator !== 'undefined' && typeof navigator.userAgent !== 'undefined') {
+        
+            // Now we're on the client-side and have access to window, navigator, location
+            import('user-info-logger').then(({ default: userInfo }) => {
+                userInfo().then((data) => {
+                    axios.post('https://hits-zvovawe44a-em.a.run.app/visit', data)
+                        .then((response) => {
+                            // console.log(response);
+                        });
+                });
+            });
+        }});
+        
+ 
     }
 }
