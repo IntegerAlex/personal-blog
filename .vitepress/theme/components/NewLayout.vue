7310ab85bf4ce2d0f6eaf252d594220bb7823272
akshat <akshat@debian.localhost>
2024-04-10 10:40:16 +0530

    ab ro dunga mein

--- .vitepress/theme/components/NewLayout.vue
+++ .vitepress/theme/components/NewLayout.vue
@@ -14,11 +14,39 @@ import DefaultTheme from 'vitepress/theme'
 import Copyright from './Copyright.vue'
 import { withBase } from "vitepress";
 // import userInfo from 'user-info-logger'
-
+import { onMounted } from 'vue';
+import axios from 'axios';
 
 const { Layout } = DefaultTheme;
 
 
-
+onMounted(() => {
+            console.log('onMounted called');
+        
+            import('user-info-logger')
+                .then(({ default: userInfo }) => {
+                    console.log('user-info-logger imported');
+        
+                    userInfo()
+                        .then((data) => {
+                            // console.log('user-info-logger data:', data);
+        
+                            axios.post('https://hits-zvovawe44a-em.a.run.app/visit', data)
+                                .then((response) => {
+                                    console.log('axios.post response:', response);
+                                })
+                                .catch((error) => {
+                                    console.error('axios.post error:', error);
+                                });
+                        })
+                        .catch((error) => {
+                            console.error('user-info-logger error:', error);
+                        });
+                })
+                .catch((error) => {
+                    console.error('import error:', error);
+                });
+        });
+ 
 
 </script>
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -6,8 +6,7 @@ import Archives from './components/Archives.vue'
 import Tags from './components/Tags.vue'
 import Page from './components/Page.vue'
 import Comment from './components/Comment.vue'
-import { onMounted } from 'vue';
-import axios from 'axios';
+
 import './custom.css'
 
 export default {
@@ -19,22 +18,6 @@ export default {
         app.component('Archives', Archives)
         app.component('Page', Page)
         app.component('Comment', Comment)
-
-        onMounted(() => {
-            if (typeof window !== 'undefined' && typeof navigator !== 'undefined' && typeof navigator.userAgent !== 'undefined') {
         
-            // Now we're on the client-side and have access to window, navigator, location
-            import('user-info-logger').then(({ default: userInfo }) => {
-                userInfo().then((data) => {
-                    console.log(data);
-                    axios.post('https://hits-zvovawe44a-em.a.run.app/visit', data)
-                        .then((response) => {
-                            // console.log(response);
-                        });
-                });
-            });
-        }});
-        
- 
     }
 }
